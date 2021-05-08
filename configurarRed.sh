d4445c70ed577912a3f5e7e492c1abe4c99f314b
Mario Román Dono <maroma06@ucm.es>
2021-05-08 14:41:54 +0200

    Agrupar instrucciones en funciones

--- configurarRed.sh
+++ configurarRed.sh
@@ -9,10 +9,8 @@ salirTrasError () {
 	exit
 }
 
-configurarIP() {
-if [ "$tipomv" = "Router" ] ; then
-	interfaz="enp0s8"
-	echo "Interfaz: ${interfaz}"
+escribirIPenArchivo() {
+	echo "Interfaz: ${1}"
 	echo "Introduce la IP: "
 	read ip
 	echo "Introduce la máscara de subred (presiona Enter si quieres que sea 255.255.255.0): "
@@ -23,42 +21,28 @@ if [ "$tipomv" = "Router" ] ; then
 	comandoIP="\nauto ${interfaz}\niface ${interfaz} inet static\naddress ${ip}\nnetmask ${mascara}\n"
 	echo -en $comandoIP >> /etc/network/interfaces || salirTrasError
 	echo "Configuración añadida al fichero /etc/network/interfaces"
+}
 
-	interfaz="enp0s9"
-	echo "Interfaz: ${interfaz}"
-	echo "Introduce la IP: "
-	read ip
-	echo "Introduce la máscara de subred (presiona Enter si quieres que sea 255.255.255.0): "
-	read mascara
-	if [ -z "$mascara"]; then
-		mascara="255.255.255.0"
-	fi
-	comandoIP="\nauto ${interfaz}\niface ${interfaz} inet static\naddress ${ip}\nnetmask ${mascara}\n"
-	echo -en $comandoIP >> /etc/network/interfaces || salirTrasError
-	echo "Configuración añadida al fichero /etc/network/interfaces"
-else
-	if [ "$so" = "Kali" ] ; then
-		interfaz="eth1"
-	else 
+configurarIP() {
+	if [ "$tipomv" = "Router" ] ; then
 		interfaz="enp0s8"
+		escribirIPenArchivo $interfaz
+
+		interfaz="enp0s9"
+		escribirIPenArchivo $interfaz
+	else
+		if [ "$so" = "Kali" ] ; then
+			interfaz="eth1"
+		else 
+			interfaz="enp0s8"
+		fi
+		escribirIPenArchivo $interfaz
 	fi
-	echo "Interfaz: ${interfaz}"
-	echo "Introduce la IP: "
-	read ip
-	echo "Introduce la máscara de subred (presiona Enter si quieres que sea 255.255.255.0): "
-	read mascara
-	if [ -z "$mascara"]; then
-		mascara="255.255.255.0"
-	fi
-	comandoIP="\nauto ${interfaz}\niface ${interfaz} inet static\naddress ${ip}\nnetmask ${mascara}\n"
-	echo -en $comandoIP >> /etc/network/interfaces || salirTrasError
-	echo "Configuración añadida al fichero /etc/network/interfaces"
-fi
-# Pasos 2 y 3
-echo "Ejecutando /etc/init.d/networking reload"
-/etc/init.d/networking reload || salirTrasError
-echo "Ejecutando /etc/init.d/networking restart"
-/etc/init.d/networking restart || salirTrasError
+	# Pasos 2 y 3
+	echo "Ejecutando /etc/init.d/networking reload"
+	/etc/init.d/networking reload || salirTrasError
+	echo "Ejecutando /etc/init.d/networking restart"
+	/etc/init.d/networking restart || salirTrasError
 }
 
 configurarRuta() {
@@ -84,6 +68,15 @@ configurarRuta() {
 		route add default gw ${gateway} || salirTrasError
 		echo "Configurada"	
 	fi
+}
+
+configurarIptables() {
+	echo "Configurando las reglas de Iptables relativas al tráfico DNS..."
+	if [ "$tipomv" = "Router" ] ; then
+		iptables -A FORWARD -p udp --dport 53 -j DROP || salirTrasError
+	fi
+	iptables -A OUTPUT -p udp --dport 53 -j DROP || salirTrasError
+	echo "Reglas iptables añadidas"
 }
 
 # Comienzo del script
@@ -160,13 +153,17 @@ if [ "$tipomv" = "Router" ] ; then
 	echo "Activado"
 fi
 
-# Paso 7
-echo "Configurando las reglas de Iptables relativas al tráfico DNS..."
-if [ "$tipomv" = "Router" ] ; then
-	iptables -A FORWARD -p udp --dport 53 -j DROP || salirTrasError
-fi
-iptables -A OUTPUT -p udp --dport 53 -j DROP || salirTrasError
-echo "Reglas iptables añadidas"
+# Paso 8: Preguntar si se desea configurar las reglas Iptables relativas al DNS
+echo "¿Quieres añadir las reglas de Iptables para restringir el tráfico DNS?"
+select opDNS in "Si" "No";
+do
+	case $opDNS in
+		Si ) configurarIptables 
+			break ;;
+		No ) break;;
+		* ) echo "Opción incorrecta"
+	esac
+done
 
 # Fin del script
 echo "Todo listo!"
